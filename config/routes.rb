c491f9ef2e3bab4cfc139a3852528fce9126e743
github username <github email address>
2017-10-26 14:50:12 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id' => 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me' => 'pages#about'
   get 'contact' => 'pages#contact'
